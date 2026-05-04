5877536c6c6fece9fff6192c957686d9a984282e
MrRichar02 <richarmamadi03@gmail.com>
2026-05-04 16:25:54 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+				clang-tools
+        gcc
+				man-pages
+        valgrind
+      ];
+    };
+  };
+}
